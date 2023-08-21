9b60696116d03f799aa7f239114ac3ff2c670260
YIJUN <u1475897@umail.utah.edu>
2023-08-21 10:30:25 -0600

    hello world complete

---
+++ hello_world/hello_world/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hello_world
+//
+//  Created by 詹怡君 on 8/21/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
