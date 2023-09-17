878b95e2ba2e8d83d5e245a2287ab405aed30584
YIJUN <u1475897@umail.utah.edu>
2023-09-16 19:45:02 -0600

    Templatize Vector

--- Day15/GrowthMyVector/GrowthMyVector/main.cpp
+++ Day15/GrowthMyVector/GrowthMyVector/main.cpp
@@ -6,46 +6,51 @@
 //
 #include "Helpers.hpp"
 #include <iostream>
-template <typename T>
-void test(MyVector<T>v1,MyVector<T>v2){
-    if(v1 < v2){
-        std::cout <<"int type v1 <= v2" <<std::endl;
-    }
-    if(v1 == v2){
-        std::cout <<"int type v1 = v2" <<std::endl;
-    }
-    if(v1 >= v2){
-        std::cout <<"int type v1 >= v2" <<std::endl;
-    }
-    if(v1 <= v2){
-        std::cout <<"int type v1 <= v2" <<std::endl;
-    }
-    if(v1 < v2){
-        std::cout <<"int type v1 < v2" <<std::endl;
-    }
-    if(v1 > v2){
-        std::cout <<"int type v1 > v2" <<std::endl;
-    }
-}
+//template <typename T>
+//void test(MyVector<T>v1,MyVector<T>v2){
+//    if(v1 < v2){
+//        std::cout <<"int type v1 <= v2" <<std::endl;
+//    }
+//    if(v1 == v2){
+//        std::cout <<"int type v1 = v2" <<std::endl;
+//    }
+//    if(v1 >= v2){
+//        std::cout <<"int type v1 >= v2" <<std::endl;
+//    }
+//    if(v1 <= v2){
+//        std::cout <<"int type v1 <= v2" <<std::endl;
+//    }
+//    if(v1 < v2){
+//        std::cout <<"int type v1 < v2" <<std::endl;
+//    }
+//    if(v1 > v2){
+//        std::cout <<"int type v1 > v2" <<std::endl;
+//    }
+//}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     MyVector<int> v1(10);
     MyVector<int> v2(10);
     //test
- // v1.data = {1,2,3,4,5};
+
     v1.pushBack(1);
-//    v1.pushBack(2);
-//    v2.pushBack(2);
-//    v2.pushBack(3);
+    v1.pushBack(2);
+    v2.pushBack(2);
+    v2.pushBack(3);
 //    test(v1,v2);
-//
-//    MyVector<char> v3(10);
-//    MyVector<char> v4(10);
-//    v1.pushBack('a');
-//    v1.pushBack('b');
-//    v2.pushBack('a');
-//    v2.pushBack('b');
+    assert(v1<v2);
+    std::cout <<"int type test pass" <<std::endl;
+
+    MyVector<char> v3(10);
+    MyVector<char> v4(10);
+    v1.pushBack('a');
+    v1.pushBack('b');
+    v2.pushBack('a');
+    v2.pushBack('b');
+    assert(v3==v4);
+    std::cout <<"char type test pass" <<std::endl;
+    
 //    test(v3,v4);
     
 //
